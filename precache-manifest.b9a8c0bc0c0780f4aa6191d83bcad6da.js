aada758693224d72a01b45dcafe7a23330e35d92
MeLlamoArroz <israel.huaman@ucsp.edu.pe>
2020-04-28 21:56:14 -0500

    Updates

---
+++ precache-manifest.b9a8c0bc0c0780f4aa6191d83bcad6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709837e102ff6e27cee25f27ffe36d76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a587432720802f466c8e",
+    "url": "/monsters-rolodex/static/css/main.94231c9c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a587432720802f466c8e",
+    "url": "/monsters-rolodex/static/js/main.5c51c94c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
